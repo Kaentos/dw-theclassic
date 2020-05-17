e7b0033ffeac7c8e926f70fb806c2c4eb16f82d4
Kaentos <miguelmagueijo@hotmail.com>
2020-05-17 23:33:43 +0100

    save progress

--- assets/js/register.js
+++ assets/js/register.js
@@ -3,6 +3,21 @@ function register () {
     let nu_email = document.getElementById("nu_email");
     let nu_password = document.getElementById("nu_password");
     let nu_cpassword = document.getElementById("nu_cpassword");
-
     showErrorTxt([nu_username, nu_email, nu_password, nu_cpassword]);
+}
+
+function checkPasswords(){
+    let pw1 = document.getElementById("nu_password");
+    let pw2 = document.getElementById("nu_cpassword");
+    let cpw_error = document.getElementById("cpw_error");
+    cpw_error.style.visibility = "Hidden";
+    if(pw1.value != pw2.value) {
+        cpw_error.style.visibility = null;
+        console.log("don't match");
+    } else {
+        pw1.style.borderColor = "green";
+        pw2.style.borderColor = "green";
+        cpw_error.style.visibility = "hidden";
+        console.log("match");
+    }
 }

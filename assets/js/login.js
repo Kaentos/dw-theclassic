623dfbb287a3a4511aceb15681c41f7beb07e1c0
Kaentos <miguelmagueijo@hotmail.com>
2020-05-17 19:06:51 +0100

    add login error info

---
+++ assets/js/login.js
@@ -0,0 +1,38 @@
+const valid_emails = ["admin@mplist.com", "teste@gmail.com"];
+const valid_passwords = ["admin", "teste"];
+var invalid_txt = null;
+
+window.onload = function() {
+    invalid_txt = document.getElementById("invalid_txt");
+    console.log(this.localStorage.getItem("logged"));
+    localStorage.removeItem("logged");
+}
+
+function login () {
+    let email_input = document.getElementById("u_email");
+    let email = email_input.value.replace(/ /g, "").toLowerCase();
+    let pw_input = document.getElementById("u_password");
+    let pw = pw_input.value;
+
+    if (checkEmail(email) && valid_emails.includes(email)) {
+        console.log("valid email");
+        let idx = valid_emails.indexOf(email);
+        if(valid_passwords[idx] === pw) {
+            console.log("valid pw");
+            localStorage.setItem("logged", true);
+        } else {
+            console.log("invalid pw");
+            invalid_txt.style.visibility = null;
+        }
+    } else {
+        console.log("Invalid email");
+        email_input.style.borderColor = "red";
+        pw_input.style.borderColor = "red";
+        invalid_txt.style.display = "block";
+    }
+}
+
+function checkEmail(email) {
+    let email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return email_regex.test(String(email));
+}

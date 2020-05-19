83d834e5b9b32dadec4636f113a4b6506a4d9831
Kaentos <miguelmagueijo@hotmail.com>
2020-05-19 21:34:05 +0100

    clean up login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,33 +1,22 @@
 function login () {
+    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
     let email_input = document.getElementById("l_email");
+    let email = email_input.value.toLowerCase();
     let pw_input = document.getElementById("l_password");
     let pw = pw_input.value;
 
-    if (checkEmail()) {
-        let idx = valid_emails.indexOf(email_input.value);
+    if (regex.test(email) && valid_emails.includes(email)) {
+        let idx = valid_emails.indexOf(email);
         if (valid_passwords[idx] === pw) {
-            console.log("Valid credentials!");
             document.getElementById("login-info").classList.add("display-none");
             email_input.style.borderColor = "green";
             pw_input.style.borderColor = "green";
             // Login
             return;
-        } else {
-            console.log("Invalid credentials!");
         }
     }
     borderRed(email_input);
     borderRed(pw_input);
     document.getElementById("login-info").classList.remove("display-none");
-}
-
-function checkEmail() {
-    let email_input = document.getElementById("l_email");
-    let email = email_input.value.toLowerCase();
-    let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (regex.test(email) && valid_emails.includes(email)) {
-        return true;
-    }
-    borderRed(email_input);
-    return false;
 }
--- assets/js/lr.js
+++ assets/js/lr.js
@@ -5,8 +5,3 @@ var valid_passwords = ["admin", "teste", "bomdia"];
 function borderRed(input) {
     input.style.borderColor = "red";
 }
-
-function checkEmail2(email) {
-    let email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return email_regex.test(String(email));
-}

ca3e07e419898fc8394e952adb1a92eae683a117
Kaentos <miguelmagueijo@hotmail.com>
2020-05-21 22:49:26 +0100

    remake of login

--- assets/js/lr.js
+++ assets/js/lr.js
@@ -1,3 +1,46 @@
+const user_obj = {
+    email: null,
+    username: null,
+    password: null,
+    creationDate: null,
+    setEmail: function(email) {
+        this.email = email;
+    },
+    setUsername: function(name) {
+        this.username = name;
+    },
+    setPassword: function(password) {
+        this.password = password;
+    },
+    setCreationDate: function() {
+        this.creationDate = new Date().getTime();
+    },
+    getEmail: function() {
+        return this.email;
+    },
+    getUsername: function() {
+        return this.username;
+    },
+    getPassword: function() {
+        return this.password;
+    },
+    getCreationDate: function() {
+        return this.creationDate;
+    }
+}
+
+const user1 = Object.assign({}, user_obj);
+const user2 = Object.assign({}, user_obj);
+user1.setEmail("admin@mypopcornlist.com");
+user1.setUsername("Admin");
+user1.setPassword("admin");
+user1.setCreationDate;
+user2.setEmail("normal_user@hotmail.com");
+user2.setUsername("Normal_User");
+user2.setPassword("normal");
+user2.setCreationDate;
+
+
 var users_info_json = {
     "username" : ["admin_admin", "teste_teste", "bomdia"],
     "email": ["admin@mplist.com", "teste@gmail.com", "bomdia@gmail.com"],

40e2743cd5766daba64dfb5d8eda88d8bfef0887
André Duarte <ansadupessoal@hotmail.com>
2020-05-22 23:55:47 +0100

    Add some movie data

---
+++ assets/js/data.js
@@ -0,0 +1,52 @@
+movies = {
+    1: {
+        name: "Endgame",
+        score: 8,
+        genres: ["Action", "Adventure", "Drama", "Sci-Fi"],
+        premiere: 1556150400,
+        producers: ["Anthony Russo", "Joe Russo"],
+        age: "12+",
+        duration: "3h 1min",
+        lang: "English"
+    },
+    2: {
+        name: "Star Wars Episode VIII - The Last Jedi",
+        score: 4,
+        genres: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
+        premiere: 1513123200,
+        producers: ["Rian Johnson"],
+        age: "12+",
+        duration: "2h 32min",
+        lang: "English"
+    },
+    3: {
+        name: "Star Wars Episode IX - The Rise of Skywalker",
+        score: 6,
+        genres: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
+        premiere: 1576713600,
+        producers: ["J.J. Abrams"],
+        age: "12+",
+        duration: "2h 22min",
+        lang: "English"
+    },
+    4: {
+        name: "The Irishman",
+        score: 8,
+        genres: ["Biography", "Crime", "Drama"],
+        premiere: 1574812800,
+        producers: ["Martin Scorsese"],
+        age: "R (16+)",
+        duration: "3h 29min",
+        lang: "English"
+    },
+    5: {
+        name: "Interstellar",
+        score: 9,
+        genres: ["Adventure", "Drama", "Sci-Fi", "Thriller"],
+        premiere: 1415232000,
+        producers: "Christopher Nolan",
+        age: "12+",
+        duration: "2h 49min",
+        lang: "English"
+    }
+}

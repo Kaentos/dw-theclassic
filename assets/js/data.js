ff08aa28c7d316142f37f90d25f9bded12922e04
Kaentos <miguelmagueijo@hotmail.com>
2020-05-25 22:12:15 +0100

    save progress

--- assets/js/data.js
+++ assets/js/data.js
@@ -53,13 +53,68 @@ const movies = {
         duration: "2h 49min",
         lang: "English",
         trailer: "qLFBcdd6Qw0"
+    },
+    6: {
+        name: "1917",
+        score: 8.3,
+        genres: ["Drama", "War"],
+        premiere: 1579737600,
+        directors: ["Sam Mendes"],
+        age: "14+",
+        duration: "1h 59min",
+        lang: "English",
+        trailer: "qLFBcdd6Qw0"
+    },
+    7: {
+        name: "Knives Out",
+        score: 7.9,
+        genres: ["Comedy", "Crime", "Drama"],
+        premiere: 1574899200,
+        directors: ["Rian Johnson"],
+        age: "12+",
+        duration: "2h 11min",
+        lang: "English",
+        trailer: "qLFBcdd6Qw0"
+    },
+    8: {
+        name: "Joker",
+        score: 8.5,
+        genres: ["Thriller", "Crime", "Drama"],
+        premiere: 1570060800,
+        directors: ["Todd Phillips"],
+        age: "14+",
+        duration: "2h 2min",
+        lang: "English",
+        trailer: "qLFBcdd6Qw0"
+    },
+    9: {
+        name: "John Wick 3 - Parabellum",
+        score: 7.5,
+        genres: ["Action", "Crime", "Thriller"],
+        premiere: 1557964800,
+        directors: ["Chad Stahelski"],
+        age: "16+",
+        duration: "2h 11min",
+        lang: "English",
+        trailer: "qLFBcdd6Qw0"
+    },
+    10: {
+        name: "I Am Mother",
+        score: 6.7,
+        genres: ["Drama", "Mystery", "Sci-Fi"],
+        premiere: 1563408000,
+        directors: ["Grant Sputore"],
+        age: "None",
+        duration: "1h 53min",
+        lang: "English",
+        trailer: "qLFBcdd6Qw0"
     }
 }
 
 const shows = {
     1: {
         name: "Game of Thrones",
-        score: 7,
+        score: 9.3,
         genres: ["Action", "Adventure", "Drama", "Fantasy", "Romance"],
         premiere: 1318809600,
         directors: ["David Nutter", "Alan Taylor"],
@@ -67,6 +122,114 @@ const shows = {
         lang: "English",
         seasons: 8,
         episodes: [10, 10, 10, 10, 10, 10, 7, 6],
+        trailer: "qLFBcdd6Qw0"
+    },
+    2: {
+        name: "Rick and Morty",
+        score: 9.2,
+        genres: ["Animation", "Adventure", "Comedy"],
+        premiere: 1386028800,
+        directors: ["Dan Harmon", "Justin Roiland"],
+        age: "None",
+        lang: "English",
+        seasons: 4,
+        episodes: [11, 10, 10, 10],
+        trailer: "qLFBcdd6Qw0"
+    },
+    3: {
+        name: "Westworld",
+        score: 8.7,
+        genres: ["Drama", "Mystery", "Sci-Fi"],
+        premiere: 1475366400,
+        directors: ["Lisa Joy", "Jonathan Nolan"],
+        age: "None",
+        lang: "English",
+        seasons: 3,
+        episodes: [10, 10, 8],
+        trailer: "qLFBcdd6Qw0"
+    },
+    4: {
+        name: "The Walking Dead",
+        score: 8.2,
+        genres: ["Drama", "Horror", "Thriller"],
+        premiere: 1288656000,
+        directors: ["Frank Darabont", "Angela Kang"],
+        age: "16+",
+        lang: "English",
+        seasons: 10,
+        episodes: [6, 13, 16, 16, 16, 16, 16, 16, 16, 16],
+        trailer: "qLFBcdd6Qw0"
+    },
+    5: {
+        name: "Vikings",
+        score: 8.5,
+        genres: ["Drama", "Action", "Adventure"],
+        premiere: 1362268800,
+        directors: ["Michael Hirst"],
+        age: "None",
+        lang: "English",
+        seasons: 6,
+        episodes: [9, 10, 10, 20, 20, 20],
+        trailer: "qLFBcdd6Qw0"
+    },
+    6: {
+        name: "The Mandalorian",
+        score: 8.7,
+        genres: ["Adventure", "Action", "Sci-Fi"],
+        premiere: 1573516800,
+        directors: ["Jon Favreau"],
+        age: "None",
+        lang: "English",
+        seasons: 1,
+        episodes: [8],
+        trailer: "qLFBcdd6Qw0"
+    },
+    7: {
+        name: "Breaking Bad",
+        score: 9.5,
+        genres: ["Crime", "Drama", "Thriller"],
+        premiere: 1200787200,
+        directors: ["Vince Gilligan"],
+        age: "16+",
+        lang: "English",
+        seasons: 5,
+        episodes: [7, 13, 13, 13, 16],
+        trailer: "qLFBcdd6Qw0"
+    },
+    8: {
+        name: "Chernobyl",
+        score: 9.4,
+        genres: ["Drama", "History", "Thriller"],
+        premiere: 1557100800,
+        directors: ["Craig Mazin"],
+        age: "None",
+        lang: "English",
+        seasons: 1,
+        episodes: [5],
+        trailer: "qLFBcdd6Qw0"
+    },
+    9: {
+        name: "Dark",
+        score: 8.7,
+        genres: ["Crime", "Drama", "Mystery"],
+        premiere: 1512086400,
+        directors: ["Baran bo Odar", "Jantje Friese"],
+        age: "None",
+        lang: "English",
+        seasons: 2,
+        episodes: [10, 8],
+        trailer: "qLFBcdd6Qw0"
+    },
+    10: {
+        name: "Mr. Robot",
+        score: 8.5,
+        genres: ["Crime", "Drama", "Triller"],
+        premiere: 1435104000,
+        directors: ["Sam Esmail"],
+        age: "None",
+        lang: "English",
+        seasons: 4,
+        episodes: [10, 12, 10, 13],
         trailer: "qLFBcdd6Qw0"
     }
 }
--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,3 +1,7 @@
+let userToList = false;
+let follow_ID = -1;
+const max = 5;
+
 window.onload = function() {
     setNavbar();
     setMobileNavbar();
@@ -9,19 +13,23 @@ window.onload = function() {
 
     if (user_id !== null && user_id.length > 0) {
         user_id = Number(user_id);
-        if (user_id !== NaN && user_id > -1 && user_id <= users_info.length) {
-            if (user && getUsername(users_info[user_id]) !== getUsername(user)){
+        if (user_id !== NaN && user_id > -1 && user_id < users_info.length) {
+            let current_user = getUserObj();
+            if (current_user && getUsername(users_info[user_id]) !== getUsername(current_user)){
                 document.getElementById("username-follow-zone").innerHTML += '<button class="follow-btn" id="follow-btn">Follow</button>';
+                follow_ID = user_id;
+                document.getElementById("username-follow-zone").addEventListener("click", followUser);
             }
-            showThisUser(users_info[user_id]);
+            userToList = getUserObjWithID(user_id);
         } else {
             window.location.href = "/";
         }
-    } else if(user) {
-        showThisUser(user);
+    } else if (user_id === null && getUserObj()) {
+        userToList = getUserObj();
     } else {
         window.location.href = "/";
     }
+    showThisUser(userToList);
 
     let user_list = document.getElementById("user-list"); // meter lista
 }
@@ -37,6 +45,113 @@ function showThisUser(user) {
     document.getElementById("total_toSee_series").innerHTML = getToWatchSeries(user).length;
     document.getElementById("total_watched_movies").innerHTML = getSeenMovies(user).length;
     document.getElementById("total_watched_series").innerHTML = getSeenMovies(user).length;
+    showFavourites(user);
+    showWatching(user);
+    showToWatch(user);
 }
 
-let t = `<button class="follow-btn" id="follow-btn">Follow</button>`;
+function showFavourites(user) {
+    let content = document.getElementById("fav");
+    let count = max;
+    let list = getFavMovies(user);
+    for (let id of list) {
+        if (count === 0) {
+            return;
+        }
+        content.innerHTML += getHTMLOf(movies[id], id, "movies");
+        count--;
+    }
+    list = getFavSeries(user);
+    if (list.length === 0) {
+        if (count === 5) {
+            showNothing(content, document.getElementById("moreFav"));
+        }
+        return;
+    }
+    for (let id of list) {
+        if (count === 0) {
+            return;
+        }
+        content.innerHTML += getHTMLOf(shows[id], id, "shows");
+        count--;
+    }
+    if (count === 5) {
+        showNothing();
+    }
+}
+function showWatching(user) {
+    let content = document.getElementById("watching");
+    let count = max;
+    let list = getWatchingSeries(user);
+    if (list.length === 0) {
+        if (count === 5) {
+            showNothing(content, document.getElementById("moreWatching"));
+        }
+        return;
+    }
+    for (let id of list) {
+        if (count === 0) {
+            return;
+        }
+        content.innerHTML += getHTMLOf(shows[id], id, "shows");
+        count--;
+    }
+}
+function showToWatch(user) {
+    let content = document.getElementById("toWatch");
+    let count = max;
+    let list = getToWatchMovies(user);
+    for (let id of list) {
+        if (count === 0) {
+            return;
+        }
+        content.innerHTML += getHTMLOf(movies[id], id, "movies");
+        count--;
+    }
+    list = getToWatchSeries(user);
+    console.log(count)
+    if (list.length === 0) {
+        if (count === 5) {
+            showNothing(content, document.getElementById("moreToWatch"));
+        }
+        return;
+    }
+    for (let id of list) {
+        if (count === 0) {
+            return;
+        }
+        content.innerHTML += getHTMLOf(shows[id], id, "shows");
+        count--;
+    }
+    
+}
+
+function getHTMLOf(obj, id, type) {
+    let ref = type === "movies" ? "movieinfo.html?id="+id : "showinfo.html?id="+id;
+    return `
+    <a href="${ref}" class="flex-col ms_ss_panel">
+        <img class="ms_ss_img" src="assets/img/${type}/${id}.jpg">
+        <div>
+            ${obj.name}
+        </div>
+        <div>
+            ${new Date(obj.premiere * 1000).getFullYear()}
+        </div>
+    </a>
+    `;
+}
+
+function followUser() {
+    if (follow_ID !== -1) {
+        console.log("follow")
+    }
+}
+
+function showNothing(content, button) {
+    button.style.display = "none";
+    content.innerHTML = `
+        <div class="no-content">
+            This user doesn't have any movies or TV Shows in this category.
+        </div>
+    `;
+}

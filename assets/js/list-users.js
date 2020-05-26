a9e92e3008b38e467d96528e6784216c2c3f26b1
Kaentos <miguelmagueijo@hotmail.com>
2020-05-26 21:11:03 +0100

    shows correct users

--- assets/js/list-users.js
+++ assets/js/list-users.js
@@ -20,56 +20,65 @@ window.onload = function() {
     }
     
     showAllUsers();
+    document.getElementById("search-btn").addEventListener("click", showAllUsers);
 }
 
 function showAllUsers() {
+    let name = document.getElementById("search-name").value.toLowerCase();
     let user_list = document.getElementById("user_list");
     user_list.innerHTML = "";
     for (let i in users_info) {
         let user = users_info[i];
-        user_list.innerHTML += `
-            <a href="profile.html?id=${i}" class="flex-line ms_ss_panel">
-                <img class="ms_ss_img" src="assets/img/Users/1.jpg">
-                <div class="user-details flex-col">
-                    <div class='username'>
-                        ${getUsername(user)}
-                    </div>
-                    <div>
-                        Followers:
-                        <span id="u_tf">${getFollowers(user).length}</span>
-                    </div>
-                </div>
-            </a>
-        `
+        if (getUsername(user).toLowerCase().includes(name)){
+            showThisUser(user_list, user, i);
+        }
     }
 }
 
 function showFollowedUsers() {
+    let name = document.getElementById("search-name").value.toLowerCase();
     let show_followers = document.getElementById("only_followers").checked;
     let user_list = document.getElementById("user_list");
     let followed_users = getFollows(logged_user);
+    let count = 0;
     user_list.innerHTML = "";
     if (show_followers && followed_users.length > 0) {
         for (let i in followed_users) {
-            let user = users_info[i];
-            user_list.innerHTML += `
-                <a href="profile.html?id=${i}" class="flex-line ms_ss_panel">
-                    <img class="ms_ss_img" src="assets/img/Users/1.jpg">
-                    <div class="user-details flex-col">
-                        <div class='username'>
-                            ${getUsername(user)}
-                        </div>
-                        <div>
-                            Followers:
-                            <span id="u_tf">${getTotalFollowers(user)}</span>
-                        </div>
-                    </div>
-                </a>
-            `;
+            let user = getUserObjWithID(i);
+            if (getUsername(user).toLowerCase().includes(name)) {
+                count++;
+                showThisUser(user_list, user, id);
+            }
+            
+        }
+        if (count === 0) {
+            showNothing(user_list);
         }
     } else if (show_followers && followed_users.length === 0) {
-        user_list.innerHTML = "<div class='needToFollow'>You don't follow anyone... maybe follow some users?</div>";
+        showNothing(user_list);
     } else {
         showAllUsers();
     }
+    console.log(count);
+}
+
+function showThisUser(list, user, id) {
+    list.innerHTML += `
+        <a href="profile.html?id=${id}" class="flex-line ms_ss_panel">
+            <img class="ms_ss_img" src="assets/img/Users/1.jpg">
+            <div class="user-details flex-col">
+                <div class='username'>
+                    ${getUsername(user)}
+                </div>
+                <div>
+                    Followers:
+                    <span id="u_tf">${getFollowers(user).length}</span>
+                </div>
+            </div>
+        </a>
+    `;
+}
+
+function showNothing(div) {
+    div.innerHTML = "<div class='needToFollow'>You don't follow anyone... maybe follow some users?</div>";
 }
--- assets/js/lr.js
+++ assets/js/lr.js
@@ -14,6 +14,13 @@ const user_obj = {
     follows: [],
     followers: []
 }
+function setUserID(obj, id) {
+    if(arguments.length === 2 && typeof(obj) === "object" && typeof(id) === "number") {
+        obj.id = id;
+    } else {
+        console.log("More than 2 argument or arguments non object or non string");
+    }
+}
 function setEmail(obj, email) {
     if(arguments.length === 2 && typeof(obj) === "object" && typeof(email) === "string") {
         obj.email = email;
@@ -131,17 +138,25 @@ const email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"
 
 if (localStorage.getItem("users_info") === null) {
     const user1 = Object.assign({}, user_obj);
+    setUserID(user1, 1);
     setEmail(user1, "admin@mypopcornlist.com");
     setUsername(user1, "Admin");
     setPassword(user1, "admin");
     setCreationDate(user1);
-    user1.fav_movies = [1,2];
-    user1.fav_series = [5,6];
+    user1.fav_movies = [1, 2, 10];
+    user1.seen_movies = [4, 5];
+    user1.toWatch_movies = [3, 9];
     const user2 = Object.assign({}, user_obj);
+    setUserID(user2 , 2);
     setEmail(user2, "normal_user@hotmail.com");
     setUsername(user2, "Normal_User");
     setPassword(user2, "normal");
     setCreationDate(user2);
+    user2.follows = [1];
+    user2.fav_movies = [3, 2, 10];
+    user2.watching_series = [7, 8];
+    user2.seen_movies = [4, 5];
+    user2.toWatch_movies = [3, 9];
     var users_info = [user1, user2];
     localStorage.setItem("users_info", JSON.stringify(users_info));
 } else {

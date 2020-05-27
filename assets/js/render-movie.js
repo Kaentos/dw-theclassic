d1225682c6308f622835ce200c0be7b94ff9f4b3
Kaentos <miguelmagueijo@hotmail.com>
2020-05-27 19:00:11 +0100

    change show and movie

--- assets/js/render-movie.js
+++ assets/js/render-movie.js
@@ -7,21 +7,21 @@ window.onload = function (){
 
 function renderMovie() {
     let id = new URLSearchParams(window.location.search).get("id");
-    let movie = getMovieById(parseInt(id));
-    if (movie == undefined) {
+    if (id === null) {
         window.location.href = "/";
-        return;
     }
+    let movie = getMovieById(Number(id));
+    document.getElementById("pageTitle").innerHTML += movie.name;
     document.getElementById("info-picture").src = `assets/img/movies/${id}.jpg`;
     document.getElementById("info-title").innerHTML = movie.name;
-    document.getElementById("info-rating").innerHTML = `Score Rating: ${movie.score}`;
-    document.getElementById("info-genres").innerHTML = `Genres: ${movie.genres.join(", ")}`;
-    document.getElementById("info-premiere").innerHTML = `Premiere Date: ${getPremiereDate(movie.premiere)}`;
-    document.getElementById("info-directors").innerHTML = `Directors: ${movie.directors.join(", ")}`;
-    // document.getElementById("info-yourrating").innerHTML = ``
-    document.getElementById("info-agerating").innerHTML = `Age rating: ${movie.age}`;
-    document.getElementById("info-duration").innerHTML = `Duration: ${movie.duration}`;
-    document.getElementById("info-language").innerHTML = `Language: ${movie.lang}`;
-    document.getElementById("yt-embed").src = `https://www.youtube.com/embed/${movie.trailer}`;
+    document.getElementById("info-rating").innerHTML += movie.score + "/10";
+    document.getElementById("info-genres").innerHTML += movie.genres.join(", ");
+    document.getElementById("info-premiere").innerHTML += getPremiereDate(movie.premiere);
+    document.getElementById("info-directors").innerHTML += movie.directors.join(", ")
+    document.getElementById("info-agerating").innerHTML += movie.age;
+    document.getElementById("info-duration").innerHTML += movie.duration;
+    document.getElementById("info-language").innerHTML += movie.lang;
+    document.getElementById("yt-embed").src = "https://www.youtube.com/embed/" + movie.trailer;
+    
 
 }
--- assets/js/render-show.js
+++ assets/js/render-show.js
@@ -7,32 +7,31 @@ window.onload = function () {
 
 function renderShow() {
     let id = new URLSearchParams(window.location.search).get("id");
-    let show = getShowById(parseInt(id));
-    if (show == undefined) {
+    if (id === null) {
         window.location.href = "/";
-        return;
     }
+    let show = getShowById(Number(id));
+    document.getElementById("pageTitle").innerHTML += show.name;
     document.getElementById("info-picture").src = `assets/img/shows/${id}.jpg`;
     document.getElementById("info-title").innerHTML = show.name;
-    document.getElementById("info-rating").innerHTML = `Score Rating: ${show.score}`;
-    document.getElementById("info-genres").innerHTML = `Genres: ${show.genres.join(", ")}`;
-    document.getElementById("info-premiere").innerHTML = `Premiere Date: ${getPremiereDate(show.premiere)}`;
-    document.getElementById("info-directors").innerHTML = `Directors: ${show.directors.join(", ")}`;
-    // document.getElementById("info-yourrating").innerHTML = ``
-    document.getElementById("info-agerating").innerHTML = `Age rating: ${show.age}`;
-    document.getElementById("info-language").innerHTML = `Language: ${show.lang}`;
-    document.getElementById("info-seasons").innerHTML = `Seasons: ${show.seasons}`;
+    document.getElementById("info-rating").innerHTML += show.score + "/10";
+    document.getElementById("info-genres").innerHTML += show.genres.join(", ");
+    document.getElementById("info-premiere").innerHTML += getPremiereDate(show.premiere);
+    document.getElementById("info-directors").innerHTML += show.directors.join(", ");
+    document.getElementById("info-agerating").innerHTML += show.age;
+    document.getElementById("info-language").innerHTML += show.lang;
+    document.getElementById("info-seasons").innerHTML += show.seasons;
     let eps = [];
     let selectbox = document.getElementById("info-episodes-select")
     for (const [i, v] of show.episodes.entries()) {
         let option = document.createElement("option");
-        option.innerHTML = `S${i+1}`;
+        option.innerHTML = `Season: ${i+1}`;
         option.value = v;
         selectbox.add(option);
     }
     selectbox.onchange = function() {
-        document.getElementById("info-episodes").innerHTML = `Episodes: ${this.value}`;
+        document.getElementById("info-episodes").innerHTML = this.value;
     }
-    document.getElementById("info-episodes").innerHTML = `Episodes: ${show.episodes[0]}`;
+    document.getElementById("info-episodes").innerHTML = show.episodes[0];
     document.getElementById("yt-embed").src = `https://www.youtube.com/embed/${show.trailer}`;
 }
